b2d688135e07c855a5b77b535e8d4d70dec547f5
Muaaz-Siddiq <muaazsiddiq98@gmail.com>
2025-03-12 08:36:04 +0100

    agg. sub qureires

--- sql_quizzes.sql
+++ sql_quizzes.sql
@@ -61,3 +61,8 @@ WHERE COUNTRYCODE = 'JPN';
 Write a query calculating the amount of error 
 (i.e.:  average monthly salaries), and round it up to the next integer.*/
 SELECT CEIL(ROUND(AVG(SALARY) - AVG(REPLACE(SALARY, '0', '')), 2)) FROM EMPLOYEES;
+
+/*We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+Then print these values as  space-separated integers.*/
+SELECT MAX(salary*months), COUNT(*) FROM EMPLOYEE
+WHERE salary*months = (SELECT MAX(salary*months) FROM EMPLOYEE);
